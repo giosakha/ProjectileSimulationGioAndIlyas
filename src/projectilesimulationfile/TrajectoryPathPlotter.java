e188248b2d17d97647053b7dde6063809265ac96
giosakha <158304545+giosakha@users.noreply.github.com>
2024-10-25 18:16:59 -0400

    giorgio: add Projectile class (setters getters that help with calculations)

---
+++ src/projectilesimulationfile/TrajectoryPathPlotter.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectilesimulationfile;
+
+import javafx.scene.shape.Path;
+
+/**
+ *
+ * @author giorg
+ */
+public class TrajectoryPathPlotter {
+   
+    private static final double timeInterval = 0.1;
+    public static Path pathTrajectory(double initVelocity,double launchAngle  , double airResistance) {
+      Path path = new Path();
+        
+        return path;
+    }
+}
