dbf05b8624c69a7d2554771b255f9ed3ad1edeeb
Ilyasistriggered <2386636@edu.vaniercollege.qc.ca>
2024-11-21 00:19:49 -0500

    Commit ilyas
    Every class inside src and change for ProjectileSimulationFile class

--- src/projectilesimulationfile/ProjectileSimulationfile.java
+++ src/projectilesimulationfile/ProjectileSimulationfile.java
@@ -5,24 +5,27 @@
 package projectilesimulationfile;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
  *
  * @author giorg
  */
-public class ProjectileSimulationfile extends Application {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-
+public class Main extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void start(Stage primaryStage) {
+        BorderPane mainPane = new BorderPane();
+        SimulationController controller = new SimulationController(mainPane);
+        Scene scene = new Scene(mainPane, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Projectile Motion Simulation");
+        primaryStage.show();
     }
-    
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
